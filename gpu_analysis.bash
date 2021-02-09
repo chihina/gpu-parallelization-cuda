e3457966cd256e4f2951f7ea5b948f13b78b87d1
chihina <program.student.tti@gmail.com>
2021-02-09 14:45:25 +0900

    upload the bash script to do execution and compile simultaneously

--- gpu_analysis.bash
+++ gpu_analysis.bash
@@ -1,9 +1,11 @@
+# compile cpp and cuda scripts
 g++ inner_product.cpp -o inner_product_cpp
 nvcc inner_product.cu -o inner_product_cuda 
 nvcc inner_product_block.cu -o inner_product_block_cuda 
 nvcc inner_product_grid.cu -o inner_product_grid_cuda 
 
+# execute cpp and cuda scripts
 ./inner_product_cpp
-nvprof ./inner_product_cuda
-nvprof ./inner_product_block_cuda
-nvprof ./inner_product_grid_cuda
+nvprof ./inner_product_cuda > inner_product_cuda.log
+nvprof ./inner_product_block_cuda > inner_product_block_cuda.log
+nvprof ./inner_product_grid_cuda > inner_product_grid_cuda.log
